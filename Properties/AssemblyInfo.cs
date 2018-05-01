76993e2b8ff9852bb0f728055647d1623d473046
Gangor <jde-sain@student.42.fr>
2018-05-01 21:53:36 +0200

    change version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gangor")]
 [assembly: AssemblyProduct("MapEditor")]
-[assembly: AssemblyCopyright("Rappelz Equation © 2017")]
+[assembly: AssemblyCopyright("Rappelz Equation © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.3")]
-[assembly: AssemblyFileVersion("1.2.2.3")]
+[assembly: AssemblyVersion("1.2.3.2")]
+[assembly: AssemblyFileVersion("1.2.3.2")]
