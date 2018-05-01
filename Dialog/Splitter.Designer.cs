864d86ef6740dc1c15ebb6389009951e880cf7d1
Gangor <jde-sain@student.42.fr>
2018-05-01 21:51:22 +0200

    add splitter

---
+++ Dialog/Splitter.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace MapEditor.Dialog
+{
+	partial class MapSeparator
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MapSeparator));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.Nametxt = new System.Windows.Forms.TextBox();
+			this.Imagetxt = new System.Windows.Forms.TextBox();
+			this.Outputtxt = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.label5 = new System.Windows.Forms.Label();
+			this.Encodetxt = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(12, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Name :";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(12, 70);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(42, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Image :";
+			// 
+			// Nametxt
+			// 
+			this.Nametxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+			this.Nametxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Nametxt.ForeColor = System.Drawing.Color.White;
+			this.Nametxt.Location = new System.Drawing.Point(76, 15);
+			this.Nametxt.Name = "Nametxt";
+			this.Nametxt.Size = new System.Drawing.Size(311, 20);
+			this.Nametxt.TabIndex = 6;
+			// 
+			// Imagetxt
+			// 
+			this.Imagetxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+			this.Imagetxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Imagetxt.ForeColor = System.Drawing.Color.White;
+			this.Imagetxt.Location = new System.Drawing.Point(76, 68);
+			this.Imagetxt.Name = "Imagetxt";
+			this.Imagetxt.ReadOnly = true;
+			this.Imagetxt.Size = new System.Drawing.Size(311, 20);
+			this.Imagetxt.TabIndex = 7;
+			this.Imagetxt.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+			// 
+			// Outputtxt
+			// 
+			this.Outputtxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+			this.Outputtxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Outputtxt.ForeColor = System.Drawing.Color.White;
+			this.Outputtxt.Location = new System.Drawing.Point(76, 94);
+			this.Outputtxt.Name = "Outputtxt";
+			this.Outputtxt.ReadOnly = true;
+			this.Outputtxt.Size = new System.Drawing.Size(311, 20);
+			this.Outputtxt.TabIndex = 9;
+			this.Outputtxt.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox3_MouseClick);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(12, 96);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(45, 13);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Output :";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(124, 154);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(138, 23);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "Ok";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(12, 122);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(48, 13);
+			this.label4.TabIndex = 12;
+			this.label4.Text = "Version :";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.ForeColor = System.Drawing.Color.White;
+			this.radioButton1.Location = new System.Drawing.Point(76, 120);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(31, 17);
+			this.radioButton1.TabIndex = 13;
+			this.radioButton1.Text = "1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Checked = true;
+			this.radioButton2.ForeColor = System.Drawing.Color.White;
+			this.radioButton2.Location = new System.Drawing.Point(113, 120);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(31, 17);
+			this.radioButton2.TabIndex = 13;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "2";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(12, 44);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(58, 13);
+			this.label5.TabIndex = 2;
+			this.label5.Text = "Encoding :";
+			// 
+			// Encodetxt
+			// 
+			this.Encodetxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+			this.Encodetxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Encodetxt.ForeColor = System.Drawing.Color.White;
+			this.Encodetxt.Location = new System.Drawing.Point(76, 42);
+			this.Encodetxt.Name = "Encodetxt";
+			this.Encodetxt.Size = new System.Drawing.Size(311, 20);
+			this.Encodetxt.TabIndex = 6;
+			// 
+			// MapSeparator
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+			this.ClientSize = new System.Drawing.Size(399, 187);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.Outputtxt);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.Imagetxt);
+			this.Controls.Add(this.Encodetxt);
+			this.Controls.Add(this.Nametxt);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MapSeparator";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Map Separator";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox Nametxt;
+		private System.Windows.Forms.TextBox Imagetxt;
+		private System.Windows.Forms.TextBox Outputtxt;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox Encodetxt;
+	}
+}
---
+++ Dialog/Splitter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MapEditor.Dialog
+{
+	public partial class MapSeparator : Form
+	{
+		public MapSeparator()
+		{
+			InitializeComponent();
+		}
+
+		private void textBox1_MouseClick(object sender, MouseEventArgs e)
+		{
+			var openFile = new OpenFileDialog();
+			openFile.Filter = "Image|*.bmp;*.jpg;*.jpeg;*.png";
+
+			if (openFile.ShowDialog() == DialogResult.OK)
+			{
+				Imagetxt.Text = openFile.FileName;
+			}
+		}
+
+		private void textBox3_MouseClick(object sender, MouseEventArgs e)
+		{
+			var openFolder = new FolderBrowserDialog();
+
+			if (openFolder.ShowDialog() == DialogResult.OK)
+			{
+				Outputtxt.Text = openFolder.SelectedPath;
+			}
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			if (!File.Exists(Imagetxt.Text))
+			{
+				MessageBox.Show("File not exist !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+			if (!Directory.Exists(Outputtxt.Text))
+			{
+				MessageBox.Show("Folder not exist !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+			try
+			{
+				var image = Image.FromFile(Imagetxt.Text);
+				int partX;
+				int partY;
+				var partHeight = 128;
+				var partWidth = 128;
+				var prefix = string.Empty;
+
+				if (radioButton2.Checked)
+				{
+					partHeight = 256;
+					partWidth = 256;
+					prefix = "v256_";
+				}
+
+				partX = image.Width / partWidth;
+				partY = image.Height / partHeight;
+
+				for (int y = 0; y < partY; y++)
+				{
+					for (int x = 0; x < partX; x++)
+					{
+						var filename = $"{prefix}{Nametxt.Text}_{y}_{x}{Encodetxt.Text}.jpg";
+						var bitmap = new Bitmap(partWidth, partHeight);
+						var graphic = Graphics.FromImage(bitmap);
+
+						graphic.DrawImage(image, new Rectangle(0, 0, partWidth, partHeight), new Rectangle(x * partWidth,y * partHeight, partWidth, partHeight), GraphicsUnit.Pixel);
+						graphic.Dispose();
+
+						bitmap.Save(Path.Combine(Outputtxt.Text, filename));
+					}
+				}
+
+				Close();
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(string.Format("MapManager::Load<Exception> -> {0}", exception), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
